4c944e790cfcbc9c20dfa1cc03eaa1bd7eadf300
caglacopurkaya <caglacopurkaya@gmail.com>
2021-11-30 22:24:52 +0300

    version 0.2

---
+++ SQTJobPortal/Models/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SQTJobPortal.Models
+{
+    public class ViewModel
+    {
+        public User companies { get; set; }
+
+        public  Category categories { get; set; }
+        public Job jobs { get; set; }
+
+        public JobRequest request { get; set; }
+ 
+     
+    }
+}
